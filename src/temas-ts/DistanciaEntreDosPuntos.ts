3a494d0dab8c30589c43150559f8844e0abc273d
Ana Paulina Pedroza Villegas <anapau1pv@gmail.com>
2024-09-21 11:07:45 -0600

    commit proyectoFinal

--- src/temas-ts/DistanciaEntreDosPuntos.ts
+++ src/temas-ts/DistanciaEntreDosPuntos.ts
@@ -11,9 +11,15 @@ export class CalcularDistancia {
         this.y2 = y2;
     }
 
-    static calcularDis(x1: number, y1: number, x2: number, y2: number): number {
+    public static calcularDis(x1: number, y1: number, x2: number, y2: number): number {
         const dx = x2 - x1;
         const dy = y2 - y1;
         return Math.sqrt(dx * dx + dy * dy);
     }
+    
 }
+
+const puntoA = { x: -1, y: 0 };
+const puntoB = { x: 1, y: 0 };
+
+console.log(`La distancia entre los dos puntos es: ${CalcularDistancia.calcularDis(puntoA.x, puntoA.y, puntoB.x, puntoB.y)}`);
